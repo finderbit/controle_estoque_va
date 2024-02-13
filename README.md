29678ce26c1cb25757cfb685ce738529a29b3c2a
finderbit <mateusjjosesiilva@gmail.com>
2024-02-13 04:07:03 -0300

    Inicio do desenvolvimento serio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # controle_estoque_va
 Sistema de controle de estoque para uma micro empresa
+
+- [ ] Controle de estoque 
--- app.py
+++ app.py
@@ -3,40 +3,17 @@ from os.path import join
 from main import readjson
 
 app = Flask(__name__)
-routes = "home", "financeiro", "estoque","cadastro"
-ROOT_MODEL = "models"
-NAME_FILE = "clientes.json"
-FILE = join(ROOT_MODEL,NAME_FILE)
-dados = readjson(FILE)
+routes = "home", "estoque", "cadastro", "financeiro",
+
 
 @app.route("/", methods=["GET"])
 @app.route("/home", methods=["GET"])
 def home():
-    return render_template("index.html", routes=routes), 200
-
-
-@app.route("/financeiro")
-def financeiro():
-    return render_template("financeiro.html",routes=routes,title_page="Financeiro"), 200
-
+    return render_template("index.html", routes=routes,title_page="ERP FINDERBIT"), 200
 
 @app.route("/estoque")
 def estoque():
-    return render_template("estoque.html",routes=routes,title_page="Estoque"), 200
-
-
-@app.route("/cadastro", methods=["GET","POST"])
-def cadastro():
-    if request.method == "GET":
-        # return dados
-        return render_template("cadastro.html",
-                               routes=routes,title_page="Cadastro",dados=dados), 200
-    if request.method == "POST":
-        return ""
-
-
-
-
+    return render_template("estoque.html", routes=routes, title_page="Estoque"), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
