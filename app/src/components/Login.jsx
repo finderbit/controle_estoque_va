0433caf9176738b19fe8563f1c940f7057850cfa
mateussiilva <mateuspython3010@gmail.com>
2024-02-23 00:10:05 -0300

    Primeira tela concluida

---
+++ app/src/components/Login.jsx
@@ -0,0 +1,20 @@
+
+
+
+function Login() {
+    return (
+        <form action="#" method="post">
+            <div>
+                <input type="text" placeholder="Enter email" />
+            </div>
+            <div>
+                <input type="text" placeholder="Enter password" />
+            </div>
+            <div>
+                <input type="submit" value="Login" />
+            </div>
+        </form>
+    )
+}
+
+export default Login;
--- server/app.py
+++ server/app.py
@@ -1,19 +1,33 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, jsonify,redirect
 from os.path import join
-from server.main import readjson
+
 
 app = Flask(__name__)
-routes = "home", "estoque", "cadastro", "financeiro",
+# routes = "home", "estoque", "cadastro", "financeiro",
+routes = "home", "estoque"
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
+def index():
+    
+    if request.method == "GET":
+        return render_template("index.html",title_page="Login")
+    
+    if request.method == "POST":
+        user_email = request.form["useremail"]
+        user_password = request.form["userpassword"]
+        if user_email == "mateussiilva" and user_password == "3010":
+            return redirect(url_for("home"))
+
+
 @app.route("/home", methods=["GET"])
 def home():
-    return render_template("index.html", routes=routes,title_page="ERP FINDERBIT"), 200
+    return render_template("home.html",title_page="Pagina Inicial",routes=routes)
 
 @app.route("/estoque")
 def estoque():
-    return render_template("estoque.html", routes=routes, title_page="Estoque"), 200
+    return render_template("estoque.html",title_page="Estoque"), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
