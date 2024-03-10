7b02142e4af120a9ceb458549e5e570884c96098
mateussiilva <mateuspython3010@gmail.com>
2024-03-09 22:31:46 -0300

    Iniciando a pagina de cadastro

--- server/cadastro_cliente/cadastro_pessoas.py
+++ server/cadastro_cliente/cadastro_pessoas.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request,redirect,url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 # from main import receber_dados
 
 bp_cadastro = Blueprint("cadastro_cliente", __name__)
@@ -19,14 +19,13 @@ tel == telefone
 @bp_cadastro.route("/cadastro", methods=["POST", "GET"])
 def cadastro():
     if request.method == "POST":
-        cliente = request.form["cliente"]
+        nome = request.form["nameclient"]
+        cpf = request.form["cpfclient"]
+        rua = request.form["rua"]
+        numero = request.form["numero"]
         bairro = request.form["bairro"]
-        # rua = request.form["rua"]
-        # cidade = request.form["cidade"]
-        tel = request.form["tel"]
-        print(tel)
-        # receber_dados(cliente,bairro,tel)
-        
-        return render_template("cadastro_pessoa.html")
+        # print(nome, cpf, rua, numero, bairro)
+        print(nome)
+        return render_template("cadastro_pessoa.html", title_page="Cadastro de Cliente")
     elif request.method == "GET":
-        return render_template("cadastro_pessoa.html",title_page="Cadastro de Cliente")
+        return render_template("cadastro_pessoa.html", title_page="Cadastro de Cliente")
